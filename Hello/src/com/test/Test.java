bf1b7098f6daa973af8ab659a4958e8738aa13de
hucj <hucj@ihczd.com>
2015-02-28 17:17:44 +0800

    second commit

---
+++ Hello/src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("input:");
+		String str = scanner.next();
+		
+		System.out.println(str);
+	}
+}
